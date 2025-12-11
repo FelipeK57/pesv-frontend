86e37d13f34b6f6bfe1ce604f6328b60cdca4c10
Kevin <kevinfelipebolanosorozco@gmail.com>
2025-12-11 14:36:34 -0500

    Update link in ReadxPresentation to direct to Login Page

--- src/app/lib/ReadxPresentation.tsx
+++ src/app/lib/ReadxPresentation.tsx
@@ -5,8 +5,8 @@ export const ReadxPresentation = () => {
     <main className="grid place-content-center gap-6 h-full">
       <img src="/Readx Icon.svg" alt="ReaDx Logo" className="w-32 mx-auto"/>
       <h1 className="text-2xl font-bold text-center">Welcome to Readx SPA</h1>
-      <Link className="underline text-[#7828C8] text-center text-sm" to="users">
-        Go to Example Users Page
+      <Link className="underline text-[#7828C8] text-center text-sm" to="login">
+        Go to Login Page
       </Link>
     </main>
   );
